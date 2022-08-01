a092b3bffd26c2003f97063746e2a276776a0c9b
ayamoosa123 <ayamoosa98@gmail.com>
2022-07-31 21:48:17 -0700

    fix new item button bug

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
-import { toDate, isToday, isThisWeel, subDays, format } from "date-fns";
+import { toDate, isToday, isThisWeek, subDays, format } from "date-fns";
 import image from "./img/x-symbol.svg";
 
 //make inbox for tasks
 // add ui for adding tasks
-
-
 const page = (name, lowercase, selector) =>{
     
   const makeTitle = () => {
@@ -16,34 +14,24 @@ const page = (name, lowercase, selector) =>{
       newContainer.id = lowercase + "-content";
       container.appendChild(h2);
       container.appendChild(newContainer);
+      if (selector.id === "inbox" ||  selector.id === "new-project"){
+        handleAddItemButton(newContainer);
+      }
     };
-  return{name, lowercase, selector, makeTitle}
+    const handleAddItemButton = (container) =>{
+      const addItemButton = document.createElement("button");
+      addItemButton.setAttribute("id","add-list-item");
+      addItemButton.textContent ="Add List Item";
+      container.appendChild(addItemButton);
+      const list = ListItems(name, container, addItemButton)
+      addItemButton.addEventListener("click", ()=>{
+        list.makeItem();
+      })
+    }
+      
+  return{name, lowercase, selector, makeTitle, handleRemoveButton}
 }
-
-const inbox = document.querySelector("#inbox");
-const today = document.querySelector("#today");
-const thisWeek = document.querySelector("#this-week");
-const archived = document.querySelector("#archived");
-
-const inboxpg = page("Inbox", "inbox", inbox);
-const todaypg = page("Today", "today", today);
-const thisWeekpg =page("This Week", "this-week", thisWeek);
-const archivedpg = page("Archived", "archive", archived);
-
-inbox.addEventListener("click", () => {
-  inboxpg.makeTitle();
-});
-today.addEventListener("click", () => {
-  todaypg.makeTitle();
-});
-thisWeek.addEventListener("click", () => {
-  thisWeekpg.makeTitle();
-});
-archived.addEventListener("click", () => {
-  archivedpg.makeTitle();
-});
-
-
+// --------------------------------------Project------------------------------//
 const Project = (addProj) => {
   const newProjPrompt = () => {
     const newProj = document.querySelector("#project-container");
@@ -100,7 +88,7 @@ const Project = (addProj) => {
 
     newProj.appendChild(projItem);
 
-    const projpg = page(name, name.toLowerCase());
+    const projpg = page(name, name.toLowerCase(), projItem.id);
     projpg.makeTitle();
 
     projItem.addEventListener("click", () => {
@@ -112,23 +100,83 @@ const Project = (addProj) => {
   return{addProj, newProjButtons, replaceProj, newProjPrompt, handleAdd}
 }
 
-const addProject = document.querySelector("#add-project");
-const addProjpg = Project(addProject);
-addProject.addEventListener("click", () => {
-  addProjpg.newProjPrompt();
-});
+//-------------------------------list-------------------------------------//
+const ListItems = (name,container, addItem) =>  {
+  const makeItem = () => {
+    const newItemPrompt = () => {
+      
+      const prompt = document.createElement("div");
+      prompt.className = "new-item-prompt";
+      prompt.innerHTML += `
+              <form>
+                <input type="text" id="item-name" name="item-name" required>
+                <input type="date" id="item-due-date" required>
+                <label for="item-due-date">Due Date</label> 
+                <button type="submit" id = "add-item">Add</button>
+                <button id = "cancel-item">Cancel</button>
+              </form>
+          `;
+      container.append(prompt);
+      newItemButtons(prompt);
+    }
+  
+    const newItemButtons = (prompt) => {
+      const addItembtn = document.querySelector("#add-item");
+      const cancelItem = document.querySelector("#cancel-item");
+      const itemName = document.querySelector("#item-name");
+      const date = document.querySelector("#item-due-date")
+  
+      addItembtn.addEventListener("click", () => {
+        handleAdd(itemName.value);
+        replaceItem(prompt);
+      });
+      // addProj.addEventListener("keypress",function (e){
+      //     if (e.key === 'Enter'){
+      //         this.handleAdd(projName.value);
+      //     }
+      // })
+      cancelItem.addEventListener("click", () => {
+        replaceItem(prompt);
+      });
+    }
+    const replaceItem = (prompt) => {
+      const newItem = document.createElement("button");
+      newItem.className = "add";
+      newItem.setAttribute("id", "add-list-item");
+      newItem.textContent = "Add List Item";
+      container.appendChild(newItem);
+      prompt.remove();
+      newItem.addEventListener("click", () => {
+        newItemPrompt();
+      });
+    }
+  
+    const handleAdd = (name) => {
+      const item = document.createElement("button");
+      item.setAttribute("id", "new-item");
+      item.textContent = name;
+      
+      container.appendChild(item);
+  
+      handleRemoveButton(item);
+    }
+    newItemPrompt(); 
+    return{newItemPrompt,  newItemButtons, replaceItem, handleAdd}
+  }
 
-
-const ListItems = () =>  {
-  // makeItem() {
-  //   const container = document.querySelector("#inbox-main-container");
-  // }
-  // sortItem() {}
-  // deleteItem() {}
+  const sortItem = () =>{
+ 
+  }
+  const deleteItem = () =>{}
+  
   // archiveItem(){
 
   // }
+  return{makeItem, sortItem, deleteItem}
 }
+
+const inboxList = ListItems("inbox", document.querySelector("#inbox-content"));
+// const projList = p;
 
 function handleRemoveButton(btn) {
   const remove = document.createElement("img");
@@ -150,3 +198,33 @@ function handleRemoveButton(btn) {
 function removeItem(btn) {
   btn.remove();
 }
+
+const inbox = document.querySelector("#inbox");
+const today = document.querySelector("#today");
+const thisWeek = document.querySelector("#this-week");
+const archived = document.querySelector("#archived");
+
+const inboxpg = page("Inbox", "inbox", inbox);
+const todaypg = page("Today", "today", today);
+const thisWeekpg =page("This Week", "this-week", thisWeek);
+const archivedpg = page("Archived", "archive", archived);
+
+inbox.addEventListener("click", () => {
+  inboxpg.makeTitle();
+});
+today.addEventListener("click", () => {
+  todaypg.makeTitle();
+});
+thisWeek.addEventListener("click", () => {
+  thisWeekpg.makeTitle();
+});
+// archived.addEventListener("click", () => {
+//   archivedpg.makeTitle();
+// });
+
+const addProject = document.querySelector("#add-project");
+const addProjpg = Project(addProject);
+addProject.addEventListener("click", () => {
+  addProjpg.newProjPrompt();
+});
+
